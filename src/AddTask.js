7be690a0689060a5c105f48b95e6175b8ad92742
Michaelpkr <92385361+Michaelpkr@users.noreply.github.com>
2023-03-21 16:26:29 -0500

    Adds useState

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,19 +1,18 @@
-let taskList = []
-
-const addsTask = () => {
-	taskList.push(document.getElementById("taskInput").value)
-	console.log(taskList)
-	return taskList
-}
+import { useState } from 'react';
 
 const AddTaskComponent = () => {
+	const [taskList, setTaskList] = useState();
+
+	const handleClick = () => {
+		setTaskList(taskList.push(document.getElementById("taskInput").value))
+	}
+
 	return(
 		<div>
 			<h2>Task List Creator</h2>
 			<input type="text" id="taskInput" placeholder="Enter Task Here">
 			</input>
-			<button onClick={addsTask}>Add Task</button>
-			<ul>{taskList.map(task =>(<li>{task}</li>))}</ul>
+			<button onClick={handleClick}>Add Task</button>
 		</div>
 	)
 }
