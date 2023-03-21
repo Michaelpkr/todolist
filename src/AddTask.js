64ccea28e4a413d9d6b616fe7032f30929a2d85f
Michaelpkr <92385361+Michaelpkr@users.noreply.github.com>
2023-03-21 15:47:55 -0500

    commit

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,18 +1,21 @@
 let taskList = []
 
+const addsTask = () => {
+	taskList.push(document.getElementById("taskInput").value)
+	console.log(taskList)
+	return taskList
+}
+
 const AddTaskComponent = () => {
 	return(
 		<div>
 			<h2>Task List Creator</h2>
-			<input type="text" id="taskInput" placeholder="Enter Task Here"></input>
+			<input type="text" id="taskInput" placeholder="Enter Task Here">
+			</input>
 			<button onClick={addsTask}>Add Task</button>
+			<ul>{taskList.map(task =>(<li>{task}</li>))}</ul>
 		</div>
 	)
-}
-
-const addsTask = () => {
-	taskList.push(document.getElementById("taskInput").value)
-	console.log(taskList)
 }
 
 export default AddTaskComponent
